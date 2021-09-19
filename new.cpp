b0460b5f897227857ac91abf0a32681b3c9ca369
Niyam Choudhari <im17niyam@gmail.com>
2021-09-19 10:53:58 +0530

    #1 try

--- new.cpp
+++ new.cpp
@@ -1,6 +1,8 @@
 # include<bits/stdc++.h>
 using namespace std;
-int main(){
-int a=2,b=5;
-cout<<"Hi! Your Answer is : "<<a+b<<".";
+int main()
+{
+    int a=2,b=5;
+    cout<<"Hi! Your Answer is : "<<a+b<<".";
+    return 0;
 }
